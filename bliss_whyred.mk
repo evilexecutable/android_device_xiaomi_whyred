32028ac9b940c7f9c458126f5a3777c5050e6531
AyanC1 <choudhury.ayan13@gmail.com>
2019-11-20 00:10:16 +0530

    whyred: Blissify

--- bliss_whyred.mk
+++ bliss_whyred.mk
@@ -31,9 +31,9 @@ $(call inherit-product, vendor/MiuiCamera/config.mk)
 # Inherit some common PixelExperience stuff.
 TARGET_BOOT_ANIMATION_RES := 1080
 TARGET_GAPPS_ARCH := arm64
-$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
+$(call inherit-product, vendor/bliss/config/common_full_phone.mk)
 
-PRODUCT_NAME := aosp_whyred
+PRODUCT_NAME := bliss_whyred
 PRODUCT_BRAND := Xiaomi
 PRODUCT_DEVICE := whyred
 PRODUCT_MANUFACTURER := Xiaomi
